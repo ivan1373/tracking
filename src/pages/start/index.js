5e2a3ba192daabbc25aef9b621f4bf74016e796e
Ivan Miloš <ivan.milos2009@gmail.com>
2022-04-10 21:03:38 +0200

    ...

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -14,18 +14,20 @@ const GET_ALL_SUNBEDS = gql`
 `;
 
 const Start = () => {
-	const { status, data, error, isFetching, isSuccess } = useQuery(
-		"sunbeds",
-		async () => {
-			return client
-				.request(GET_ALL_SUNBEDS)
-				.then((data) => data)
-				.catch((error) => console.log(error));
-		}
-	);
+	const { status, data, error, isFetching } = useQuery("sunbeds", async () => {
+		return client
+			.request(GET_ALL_SUNBEDS)
+			.then((data) => data)
+			.catch((error) => console.log(error));
+	});
 	return (
 		<Box>
 			<Typography variant="h4">Sunbeds</Typography>
+			<ul>
+				{data?.allsunbeds?.map((sunbed, i) => (
+					<li>{sunbed.latitude}</li>
+				))}
+			</ul>
 		</Box>
 	);
 };
